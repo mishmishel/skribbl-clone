54412a0e6ce4d15aedac59f436acc9c9cefa0239
mishmishel <misheltyurkova@gmail.com>
2026-02-27 12:23:31 +1100

    Added server entry point and Socket.io event handlers

---
+++ server/index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+const { createRoom, joinRoom, getRoom, nextTurn, checkGuess } = require('./gameState');
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: '*' } });
+
+io.on('connection', (socket) => {
+
+  socket.on('create-room', ({ username }, callback) => {
+    const room = createRoom(socket.id, username);
+    socket.join(room.code);
+    callback({ roomCode: room.code, playerId: socket.id });
+  });
+
+  socket.on('join-room', ({ roomCode, username }, callback) => {
+    const room = joinRoom(roomCode, socket.id, username);
+    if (!room) return callback({ error: 'Room not found' });
+    socket.join(roomCode);
+    io.to(roomCode).emit('room-update', room);
+    callback({ room });
+  });
+
+  socket.on('start-game', ({ roomCode }) => {
+    const room = nextTurn(roomCode);
+    io.to(roomCode).emit('game-started', room);
+    startTurnTimer(roomCode);
+  });
+
+  socket.on('draw', ({ roomCode, drawData }) => {
+    socket.to(roomCode).emit('draw', drawData); // broadcast to everyone else
+  });
+
+  socket.on('guess', ({ roomCode, guess, username }) => {
+    const result = checkGuess(roomCode, guess, socket.id);
+    if (result.correct) {
+      io.to(roomCode).emit('correct-guess', { username, scores: result.scores });
+    } else {
+      io.to(roomCode).emit('chat-message', { username, message: guess });
+    }
+  });
+
+  socket.on('disconnect', () => { /* handle player leaving */ });
+});
+
+function startTurnTimer(roomCode) {
+  let timeLeft = 80;
+  const interval = setInterval(() => {
+    io.to(roomCode).emit('timer', { timeLeft });
+    timeLeft--;
+    if (timeLeft < 0) {
+      clearInterval(interval);
+      const room = nextTurn(roomCode);
+      if (room.gameOver) {
+        io.to(roomCode).emit('game-over', room);
+      } else {
+        io.to(roomCode).emit('next-turn', room);
+        startTurnTimer(roomCode);
+      }
+    }
+  }, 1000);
+}
+
+server.listen(3001, () => console.log('Server running on port 3001'));
