76b9825293d23b1fa319fb91e8b3335e1e4f3eb8
mishmishel <misheltyurkova@gmail.com>
2026-03-06 21:23:13 +1100

    Updated socket URL for production

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,2 +1,6 @@
 import { io } from 'socket.io-client'
-export const socket = io('http://localhost:3001')
+const URL = import.meta.env.MODE === 'production' 
+  ? 'https://skribbl-clone-1-b4sg.onrender.com'
+  : 'http://localhost:3001'
+
+export const socket = io(URL)
