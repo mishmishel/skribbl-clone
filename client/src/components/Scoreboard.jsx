69f3b0081acd1afa2e2acdec752aad7b55a13efc
mishmishel <misheltyurkova@gmail.com>
2026-03-06 20:45:36 +1100

    Game over styling

--- client/src/components/Scoreboard.jsx
+++ client/src/components/Scoreboard.jsx
@@ -1,17 +1,23 @@
 function Scoreboard({ finalScores, onGoHome }) {
     return (
-        <div>
-            <h1>Scoreboard</h1>
-            <ul>
+      <div className="page">
+        <h1 className="page-title">Game Over</h1>
+        
+        <div className="card" style={{ width: '400px', marginBottom: '24px' }}>
+          <ul style={{ listStyle: 'none' }}>
             {finalScores.sort((a, b) => b.score - a.score).map((player, index) => (
-            <li key={player.id}>
+              <li key={player.id} className="player-item" style={{ fontSize: '22px', padding: '8px 0' }}>
                 {index === 0 ? '🥇' : index === 1 ? '🥈' : index === 2 ? '🥉' : `${index + 1}.`} {player.username}: {player.score}
-            </li>
+              </li>
             ))}
-            </ul>
-            <button onClick={onGoHome}>Go Home</button>
+          </ul>
         </div>
+  
+        <div style={{ width: '400px', display: 'flex', justifyContent: 'center' }}>
+          <button className='btn btn-grey' onClick={onGoHome}>home</button>
+        </div>
+      </div>
     )
-}
-
-export default Scoreboard
+  }
+  
+  export default Scoreboard
